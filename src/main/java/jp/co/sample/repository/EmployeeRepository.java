60c338e536086e66f3fed11e152acd2a7b39769c
mitsu-mitsui <yuiko.mitsui@rakus.co.jp>
2020-06-17 11:55:01 +0900

    repository 従業員Row_mapper実装完了

--- src/main/java/jp/co/sample/repository/EmployeeRepository.java
+++ src/main/java/jp/co/sample/repository/EmployeeRepository.java
@@ -1,5 +1,20 @@
 package jp.co.sample.repository;
 
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import jp.co.sample.domain.Employee;
+
+@Repository
 public class EmployeeRepository {
+
+	private static final RowMapper<Employee> EMPLOYEE_ROW_MAPPER = (rs, i) -> {
+		Employee emp = new Employee(rs.getInt("id"), rs.getString("name"), rs.getString("image"),
+				rs.getString("gender"), rs.getDate("hire_date"), rs.getString("mailAddress"), rs.getString("zip_code"),
+				rs.getString("address"), rs.getString("telephone"), rs.getInt("salary"),
+				rs.getString("characteristics"), rs.getInt("dependents_count"));
+
+		return emp;
+	};
 
 }
