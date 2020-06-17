84177be5b229e626f58d740a274c78b4e222ee1d
mitsu-mitsui <yuiko.mitsui@rakus.co.jp>
2020-06-17 13:49:04 +0900

    controller methodのno完了

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,5 +1,39 @@
 package jp.co.sample.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.service.AdministratorService;
+
+/**
+ * 従業員のコントローラ．
+ * 
+ * @author yuiko.mitsui
+ *
+ */
+@Controller
+@RequestMapping("/")
 public class AdministratorController {
+
+	@Autowired
+	private AdministratorService adminService;
+
+	@ModelAttribute
+	public InsertAdministratorForm setUpAdministratorForm() {
+		return new InsertAdministratorForm();
+	}
+
+	/**
+	 * insert後の画面遷移.
+	 * 
+	 * @return insert後画面
+	 */
+	@RequestMapping("/toInsert")
+	public String toInsert() {
+		return "administrator/insert";
+	}
 
 }
