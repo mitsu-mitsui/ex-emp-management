cff1e25b5b2c0254a91fda966d5f26a0b19508e7
mitsu-mitsui <yuiko.mitsui@rakus.co.jp>
2020-06-17 11:42:48 +0900

    repository 管理者の認証と管理者情報の取得の実装完了

--- src/main/java/jp/co/sample/repository/AdministratorRepository.java
+++ src/main/java/jp/co/sample/repository/AdministratorRepository.java
@@ -3,6 +3,7 @@ package jp.co.sample.repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
@@ -48,11 +49,19 @@ public class AdministratorRepository {
 	 * 
 	 * @param mailAddress
 	 * @param password
-	 * @return Administrator型の管理者
+	 * @return admin Administrator型の管理者
 	 */
 	public Administrator findByMailAddressAndPassword(String mailAddress, String password) {
 
-		return null;//
+		String sql = "SELECT id,name,mail_address,password" + " FROM administrators"
+				+ " WHERE mail_address=:mailAddress AND password=:password" + ";";
+
+		SqlParameterSource param = new MapSqlParameterSource().addValue("mailAddress", mailAddress).addValue("password",
+				password);
+
+		Administrator admin = template.queryForObject(sql, param, ADMINISTRATOR_ROW_MAPPER);
+
+		return admin;
 	}
 
 }
