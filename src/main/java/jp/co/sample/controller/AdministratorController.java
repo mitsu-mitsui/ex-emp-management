218484a222dba1965d4a6fb10d17877833824089
mitsu-mitsui <yuiko.mitsui@rakus.co.jp>
2020-06-17 14:18:39 +0900

    controller 管理者追加機能、画面遷移（仮）を行う処理の実装

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
 import jp.co.sample.service.AdministratorService;
 
@@ -27,13 +28,31 @@ public class AdministratorController {
 	}
 
 	/**
-	 * insert後の画面遷移.
+	 * 管理者追加画面へ遷移.
 	 * 
-	 * @return insert後画面
+	 * @return 管理者追加画面
 	 */
 	@RequestMapping("/toInsert")
 	public String toInsert() {
 		return "administrator/insert";
+	}
+	
+	/**
+	 * 管理者を追加．
+	 * @param insertAdminForm
+	 * @return 追加後の画面
+	 */
+	@RequestMapping("/insert")
+	public String insert( InsertAdministratorForm insertAdminForm ) {
+		Administrator admin = new Administrator();
+		
+		admin.setName(insertAdminForm.getName());
+		admin.setMailAddress(insertAdminForm.getMailAddress());
+		admin.setPassword(insertAdminForm.getPassword());
+		
+		adminService.insert(admin);
+		
+		return "redirect: /";
 	}
 
 }
