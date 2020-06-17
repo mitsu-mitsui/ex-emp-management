b107887fa96438323e6ece3f8751e64aa860385f
mitsu-mitsui <yuiko.mitsui@rakus.co.jp>
2020-06-17 12:03:42 +0900

    repository 従業員処理の空methodを作成、javadocの作成

--- src/main/java/jp/co/sample/repository/EmployeeRepository.java
+++ src/main/java/jp/co/sample/repository/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package jp.co.sample.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -12,7 +14,7 @@ public class EmployeeRepository {
 
 	@Autowired
 	private NamedParameterJdbcTemplate template;
-	
+
 	private static final RowMapper<Employee> EMPLOYEE_ROW_MAPPER = (rs, i) -> {
 		Employee emp = new Employee(rs.getInt("id"), rs.getString("name"), rs.getString("image"),
 				rs.getString("gender"), rs.getDate("hire_date"), rs.getString("mailAddress"), rs.getString("zip_code"),
@@ -21,7 +23,34 @@ public class EmployeeRepository {
 
 		return emp;
 	};
-	
-	
 
+	/**
+	 * 全件検索．
+	 * 
+	 * @return list 全従業員のemployee型リスト
+	 */
+	public List<Employee> findAll() {
+
+		return null;///
+	}
+
+	/**
+	 * paramによる1件検索．
+	 * 
+	 * @param id
+	 * @return id一致のemployee
+	 */
+	public Employee load(Integer id) {
+
+		return null;///
+	}
+
+	/**
+	 * idによる従業員データ更新．
+	 * 
+	 * @param emp
+	 */
+	public void update(Employee emp) {
+
+	}
 }
