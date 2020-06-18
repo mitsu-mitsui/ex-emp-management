8ebcff517b31729469a985647dbcebba89dc786e
mitsu-mitsui <yuiko.mitsui@rakus.co.jp>
2020-06-18 10:52:07 +0900

    リンクに問題あり以外は一覧表示完了

--- src/main/java/jp/co/sample/repository/EmployeeRepository.java
+++ src/main/java/jp/co/sample/repository/EmployeeRepository.java
@@ -26,7 +26,7 @@ public class EmployeeRepository {
 
 	private static final RowMapper<Employee> EMPLOYEE_ROW_MAPPER = (rs, i) -> {
 		Employee emp = new Employee(rs.getInt("id"), rs.getString("name"), rs.getString("image"),
-				rs.getString("gender"), rs.getDate("hire_date"), rs.getString("mailAddress"), rs.getString("zip_code"),
+				rs.getString("gender"), rs.getDate("hire_date"), rs.getString("mail_address"), rs.getString("zip_code"),
 				rs.getString("address"), rs.getString("telephone"), rs.getInt("salary"),
 				rs.getString("characteristics"), rs.getInt("dependents_count"));
 
