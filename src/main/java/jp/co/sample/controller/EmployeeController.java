8d66e271730665955169b0e68241a68702db11a2
mitsu-mitsui <yuiko.mitsui@rakus.co.jp>
2020-06-18 10:01:02 +0900

    empController　空のクラスを作成

---
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package jp.co.sample.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.service.EmployeeService;
+
+/**
+ * 従業員情報を操作するコントローラ．
+ * 
+ * @author yuiko.mitsui
+ *
+ */
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+	@Autowired
+	private EmployeeService empService;
+
+}
